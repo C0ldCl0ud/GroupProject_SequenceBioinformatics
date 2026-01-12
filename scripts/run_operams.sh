b185358ef85c81fbc2d6cfff5dac224f87b01e7b
C0ldCl0ud <lang.m.lang.m@googlemail.com>
2026-01-12 22:22:56 +0100

    Update run_operams.sh

--- scripts/run_operams.sh
+++ scripts/run_operams.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+# Wrapper to ensure OPERA-MS can find Racon, Minimap2, MUMmer from the host conda env
 
+# Dynamically use the conda env that is currently active
+if [ -n "$CONDA_PREFIX" ]; then
+    export PATH="$CONDA_PREFIX/bin:$PATH"
+fi
+
+# Call OPERA-MS with all arguments passed
+perl /operams/OPERA-MS.pl "$@"
