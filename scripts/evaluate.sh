510a3bcdf53c64f65dc881e04a33c6e995caf8a8
Philipp Preetz <150606047+def-user@users.noreply.github.com>
2026-01-23 17:55:42 +0100

    Update evaluate.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-find . -type d -name '*_check*' | sed 's|^\./||; s|_check$||' > paths.eval
+find . -type f -path '*_check/quality_report.tsv' | sed 's|^\./||; s|_check/quality_report.tsv$||' > paths.eval
 python3 evaluation.py > data.eval
