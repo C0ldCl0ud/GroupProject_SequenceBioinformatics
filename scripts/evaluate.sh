9100a47a174eee04e94f6cbae8e18e516299cf18
Philipp Preetz <150606047+def-user@users.noreply.github.com>
2026-01-26 06:09:11 +0100

    Update evaluate.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-find . -type f -path '*_check/quality_report.tsv' | sed 's|^\./||; s|_check/quality_report.tsv$||' > paths.eval
+comm -3 <(find . -type f -name 'tRNA_count.txt' -printf '%h\n' | sort -u) <(find . -type f -name 'quality_report.tsv' -path '*/_check/quality_report.tsv' -printf '%h\n' | sed 's@/_check@@' | sort -u) | sed 's|^\./||; s|_check/quality_report.tsv$||' > paths.eval
 python3 evaluation.py > data.eval
